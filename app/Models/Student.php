504c706ecb5394b627bf53c574f4ecf8f57330ee
KinaraAurelya <kinarasyahna@gmail.com>
2025-10-30 10:52:01 +0700

    Add Migration Student

---
+++ app/Models/Student.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factoies\HasFactory;
+
+class Student extends Model
+{
+    
+    use HasFactory;
+
+    protected $fillable = [
+        'nis',
+        'nama_lengkap',
+        'jenis_kelamin',
+        'nisn',
+    ];
+}
